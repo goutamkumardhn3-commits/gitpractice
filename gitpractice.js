52044135b944f75a4619b4908c95a28b1374debf
goutamkumardhn3 <goutamkumardhn3@gmail.com>
2026-08-16 00:11:09 +0530

    1st feature commit

--- gitpractice.js
+++ gitpractice.js
@@ -1,7 +1,2 @@
-console.log("1st main commit");
-console.log("2nd  main commit");
-console.log("3rd  main commit");
-console.log("4th  main commit");
-console.log("5th  main commit");
-console.log("4th  feature commit");
 console.log("1st feature commit");
+
